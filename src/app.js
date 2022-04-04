fe21abad7406d87ec61dd4f9b71eeee6290d109e
gurkanozer <grknzr2609@gmail.com>
2022-04-04 14:52:38 +0300

    bug fixing

--- src/app.js
+++ src/app.js
@@ -1,56 +1,54 @@
 import "./style/main.css";
-import Swiper, { Pagination } from 'swiper';
-import 'swiper/css';
-import 'swiper/css/pagination';
+import Swiper, { Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
 
-const body = document.querySelector('body');
-const overlay = document.querySelector('.overlay');
-const hamburger = document.querySelector('.hamburger');
-const navbar = document.querySelector('.header__nav');
-const header = document.querySelector('header');
+const body = document.querySelector("body");
+const overlay = document.querySelector(".overlay");
+const hamburger = document.querySelector(".hamburger");
+const navbar = document.querySelector(".header__nav");
+const header = document.querySelector("header");
 
 //MENU ACTION
-hamburger.addEventListener('click', (event) => {
-  hamburger.classList.toggle('active');
-  if(hamburger.classList.contains('active')){
-    overlay.classList.add('active');
-    body.classList.add('overlay--active');
-    navbar.classList.add('active');
-  }
-  else{
-    overlay.classList.remove('active');
-    body.classList.remove('overlay--active');
-    navbar.classList.remove('active');
+hamburger.addEventListener("click", (event) => {
+  hamburger.classList.toggle("active");
+  if (hamburger.classList.contains("active")) {
+    overlay.classList.add("active");
+    body.classList.add("overlay--active");
+    navbar.classList.add("active");
+  } else {
+    overlay.classList.remove("active");
+    body.classList.remove("overlay--active");
+    navbar.classList.remove("active");
   }
 });
-overlay.addEventListener('click',()=>{
-  overlay.classList.remove('active');
-  body.classList.remove('overlay--active');
-  navbar.classList.remove('active');
-  hamburger.classList.remove('active');
+overlay.addEventListener("click", () => {
+  overlay.classList.remove("active");
+  body.classList.remove("overlay--active");
+  navbar.classList.remove("active");
+  hamburger.classList.remove("active");
 });
 
 // Scroll Event
-document.addEventListener("scroll",()=>{
+document.addEventListener("scroll", () => {
   let scrollPos = window.scrollY;
-  if(scrollPos > 160){
-      header.classList.add("scrolled");
-      }
-  else{
-      header.classList.remove("scrolled");
-    }    
+  if (scrollPos > 160) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
 });
 
 //Window Resize
 
-window.addEventListener('resize', ()=>{
+window.addEventListener("resize", () => {
   let width = window.innerWidth;
-  if(width> 768){
-    overlay.classList.remove('active');
-    body.classList.remove('overlay--active');
-    navbar.classList.remove('active');
-    hamburger.classList.remove('active');
-    }
+  if (width > 768) {
+    overlay.classList.remove("active");
+    body.classList.remove("overlay--active");
+    navbar.classList.remove("active");
+    hamburger.classList.remove("active");
+  }
 });
 //INIT SWIPER
 Swiper.use([Pagination]);
@@ -59,17 +57,16 @@ const swiper = new Swiper(".swiper", {
   spaceBetween: 1,
   loop: true,
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
   },
   breakpoints: {
-    // when window width is >= 320px
-    375:{
-      slidesPerWiew:1,
-      spaceBetween:10
+    375: {
+      slidesPerWiew: 3,
+      spaceBetween: 10,
     },
-    768:{
-      slidesPerView:3,
-      spaceBetween:20
-    }
-  }
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
 });
